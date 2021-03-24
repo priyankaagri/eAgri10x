1441d0f7b2e46cc3d6555089f8680728bafc6883
Admin <priyanka.auti@agri10x.com>
2021-03-24 11:21:52 +0530

    initial

--- app/src/main/java/com/mobile/agri10x/models/GetBookOrder.java
+++ app/src/main/java/com/mobile/agri10x/models/GetBookOrder.java
@@ -1,4 +1,29 @@
 package com.mobile.agri10x.models;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class GetBookOrder {
+    @SerializedName("message")
+    @Expose
+    private String message;
+    @SerializedName("data")
+    @Expose
+    private GetBookOrderData data;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public GetBookOrderData getData() {
+        return data;
+    }
+
+    public void setData(GetBookOrderData data) {
+        this.data = data;
+    }
 }
